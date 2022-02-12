dc8204ccfe7f1e38352bb2ff7b9453148efa8da0
shiva-tx <rajitha.frisco@gmail.com>
2022-02-11 21:54:55 -0600

    code refactored

--- demo/src/test1/Print.java
+++ demo/src/test1/Print.java
@@ -4,11 +4,13 @@ import org.testng.annotations.Test;
 
 @Test
 public class Print {
-	
 
 //	public static void main(String[] args) 
 	{
-	
-System.out.println("Welcome Java");	}
+
+		System.out.println("Welcome Java");
+		
+		System.out.println("hey for git");
+	}
 
 }
